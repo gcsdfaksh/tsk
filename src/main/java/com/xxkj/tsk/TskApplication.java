de9b694cab978f834e5230a1cd13da34ddaaff17
xxkj ss <xxkj@1066562980@qq.com>
2021-04-24 22:50:33 +0800

    tsk one

---
+++ src/main/java/com/xxkj/tsk/TskApplication.java
@@ -0,0 +1,13 @@
+package com.xxkj.tsk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TskApplication.class, args);
+    }
+
+}
